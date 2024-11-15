35a152ce7f7ef7667904caab69442a2b73deb1ad
laurakamalyan <kamalyanlaura095@gmail.com>
2024-11-15 16:37:06 +0400

    test

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,17 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+//Route::get('/users', [UserController::class, 'index'])->name('user.index');
+//Route::post('/users', [UserController::class, 'create'])->name('user.create');
+//Route::put('/users/{id}', [UserController::class, 'update'])->name('user.update');
+//Route::delete('/users/{id}', [UserController::class, 'delete'])->name('user.delete');
+
+Route::group(['prefix' => 'users'], function () {
+    Route::get('/', [UserController::class, 'index'])->name('user.index');
+    Route::post('/', [UserController::class, 'create'])->name('user.create');
+    Route::put('/test', [UserController::class, 'test']);
+    Route::put('/{id}', [UserController::class, 'update'])->name('user.update');
+    Route::delete('/{id}', [UserController::class, 'delete'])->name('user.delete');
 });
