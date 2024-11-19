416dc74e46d75b3e5447598de0cb7ed43029f33f
Gurgen Andreasyan <gurgenandreasyan77@gmail.com>
2024-11-19 18:13:57 +0400

    Laravel Eloquent

---
+++ database/seeders/SpecificationsSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Specification;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class SpecificationsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('specifications')->insert([
+            [
+                'title' => 'qa',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'title' => 'pm',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'title' => 'developer',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+        ]);
+    }
+}
